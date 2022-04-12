579a0bbd6e9ecbb5654af569b859521380af30c6
hojmax <axelhojmark@gmail.com>
2022-04-12 18:28:11 +0200

    dbErr as higher order function

--- customer/router.js
+++ customer/router.js
@@ -14,7 +14,7 @@ const router = express.Router()
 
 const notFoundErr = res => res.status(404).send('Customer not found')
 const joiErr = (res, err) => res.status(400).send(err.details[0].message)
-const dbErr = (res, err) => res.status(500).send(err.toString())
+const dbErr = res => err => res.status(500).send(err.toString())
 
 const joiMiddleware = (schema, property) => (req, res, next) => {
     const { error } = schema(req[property])
@@ -27,25 +27,25 @@ router.use('/:id', joiMiddleware(validateId, 'params'))
 router.get('/:id', (req, res) => {
     getCustomer(req.params.id)
         .then(data => data.length == 0 ? notFoundErr(res) : res.send(data[0]))
-        .catch(err => dbErr(res, err))
+        .catch(dbErr(res))
 })
 
 router.delete('/:id', (req, res) => {
     deleteCustomer(req.params.id)
         .then(data => data.rowCount == 0 ? notFoundErr(res) : res.send('Success'))
-        .catch(err => dbErr(res, err))
+        .catch(dbErr(res))
 })
 
 router.post('/', joiMiddleware(validatePost, 'body'), (req, res) => {
     insertCustomer(req.body)
         .then(data => res.send(data[0]))
-        .catch(err => dbErr(res, err))
+        .catch(dbErr(res))
 })
 
 router.put('/:id', joiMiddleware(validatePut, 'body'), (req, res) => {
     updateCustomer(req.params.id, req.body)
         .then(data => data.rowCount == 0 ? notFoundErr(res) : res.send('Success'))
-        .catch(err => dbErr(res, err))
+        .catch(dbErr(res))
 })
 
 module.exports = router
