866d54aadc3bc9e45d0c3fb71e61d1781a0a4295
hojmax <axelhojmark@gmail.com>
2022-04-12 16:02:08 +0200

    Fixed middleware bug

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const customerRouter = require('./customer/router.js')
 const app = express()
 const port = 3000
 
+app.use(express.json())
 app.use(logger('dev', { skip: () => process.env.NODE_ENV == 'test' }));
 app.use('/customer', customerRouter)
-app.use(express.json())
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
 
