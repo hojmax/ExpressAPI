a1b9d9fda332e7e93be77c8272d8bc82210e5146
hojmax <axelhojmark@gmail.com>
2022-04-13 14:58:55 +0200

    Added testing of login

--- login/validation.js
+++ login/validation.js
@@ -3,6 +3,7 @@ const Joi = require('joi')
 const loginSchema = Joi.object({
     email: Joi.string()
         .email()
+        .max(255)
         .required(),
     password: Joi.string()
         .min(6)
--- test.js
+++ test.js
@@ -6,6 +6,66 @@ const _ = require('lodash')
 
 chai.use(chaiHttp)
 
+let bearerToken = undefined
+
+describe('/login', () => {
+    describe('.post(/)', () => {
+        it('Successful login', done => {
+            chai.request(app)
+                .post('/login')
+                .send({
+                    email: 'test@test.dk',
+                    password: 'string',
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(200)
+                    expect(res.text).to.be.a('string')
+                    bearerToken = `Bearer ${res.text}`
+                    done()
+                })
+        })
+        it('Wrong email', done => {
+            chai.request(app)
+                .post('/login')
+                .send({
+                    email: 'test@tost.dk',
+                    password: 'string',
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(401)
+                    expect(res.text).to.equal('Invalid email or password')
+                    done()
+                })
+        })
+        it('Wrong password', done => {
+            chai.request(app)
+                .post('/login')
+                .send({
+                    email: 'test@test.dk',
+                    password: 'strong',
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(401)
+                    expect(res.text).to.equal('Invalid email or password')
+                    done()
+                })
+        })
+        it('Invalid email', done => {
+            chai.request(app)
+                .post('/login')
+                .send({
+                    email: '@test.dk',
+                    password: 'string',
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(400)
+                    expect(res.text).to.equal('"email" must be a valid email')
+                    done()
+                })
+        })
+    })
+})
+
 describe('/customer', () => {
     let newCustomerId = undefined
     const testCustomer = {
@@ -23,6 +83,7 @@ describe('/customer', () => {
         it('Alter city', done => {
             chai.request(app)
                 .put(`/customer/${testCustomer.customer_id}`)
+                .set('authorization', bearerToken)
                 .send({ city: 'test_city' })
                 .end((err, res) => {
                     expect(res).to.have.status(200)
@@ -33,6 +94,7 @@ describe('/customer', () => {
         it('Alter city to previous value', done => {
             chai.request(app)
                 .put(`/customer/${testCustomer.customer_id}`)
+                .set('authorization', bearerToken)
                 .send(_.pick(testCustomer, 'city'))
                 .end((err, res) => {
                     expect(res).to.have.status(200)
@@ -43,6 +105,7 @@ describe('/customer', () => {
         it('Handle non-integer id', done => {
             chai.request(app)
                 .put(`/customer/a`)
+                .set('authorization', bearerToken)
                 .send({ city: 'test_city' })
                 .end((err, res) => {
                     expect(res).to.have.status(400)
@@ -53,6 +116,7 @@ describe('/customer', () => {
         it('Handle invalid column', done => {
             chai.request(app)
                 .put(`/customer/${testCustomer.customer_id}`)
+                .set('authorization', bearerToken)
                 .send({ invalid_property: true })
                 .end((err, res) => {
                     expect(res).to.have.status(400)
@@ -63,6 +127,7 @@ describe('/customer', () => {
         it('Handle non-existent customer', done => {
             chai.request(app)
                 .put(`/customer/99999999`)
+                .set('authorization', bearerToken)
                 .send({ city: 'test_city' })
                 .end((err, res) => {
                     expect(res).to.have.status(404)
@@ -75,6 +140,7 @@ describe('/customer', () => {
         it('Missing first_name', done => {
             chai.request(app)
                 .post('/customer')
+                .set('authorization', bearerToken)
                 .send(_.omit(testCustomer, ['first_name', 'customer_id']))
                 .end((err, res) => {
                     expect(res).to.have.status(400)
@@ -85,6 +151,7 @@ describe('/customer', () => {
         it('Missing last_name', done => {
             chai.request(app)
                 .post('/customer')
+                .set('authorization', bearerToken)
                 .send(_.omit(testCustomer, ['last_name', 'customer_id']))
                 .end((err, res) => {
                     expect(res).to.have.status(400)
@@ -95,6 +162,7 @@ describe('/customer', () => {
         it('Missing email', done => {
             chai.request(app)
                 .post('/customer')
+                .set('authorization', bearerToken)
                 .send(_.omit(testCustomer, ['email', 'customer_id']))
                 .end((err, res) => {
                     expect(res).to.have.status(400)
@@ -105,6 +173,7 @@ describe('/customer', () => {
         it('Badly formatted email', done => {
             chai.request(app)
                 .post('/customer')
+                .set('authorization', bearerToken)
                 .send({
                     first_name: 'test_first_name',
                     last_name: 'test_last_name',
@@ -119,6 +188,7 @@ describe('/customer', () => {
         it('Handle invalid column', done => {
             chai.request(app)
                 .post('/customer')
+                .set('authorization', bearerToken)
                 .send({
                     invalid_property: true,
                     first_name: 'test_first_name',
@@ -134,6 +204,7 @@ describe('/customer', () => {
         it('Create new customer', done => {
             chai.request(app)
                 .post('/customer')
+                .set('authorization', bearerToken)
                 .send(_.omit(testCustomer, 'customer_id'))
                 .end((err, res) => {
                     expect(res.body).to.have.own.property('customer_id')
@@ -148,6 +219,7 @@ describe('/customer', () => {
         it('Delete newly created customer', done => {
             chai.request(app)
                 .delete(`/customer/${newCustomerId}`)
+                .set('authorization', bearerToken)
                 .end((err, res) => {
                     expect(res).to.have.status(200)
                     expect(res.text).to.equal('Success')
@@ -157,6 +229,7 @@ describe('/customer', () => {
         it('Error when deleting non-existent customer', done => {
             chai.request(app)
                 .delete(`/customer/${newCustomerId}`)
+                .set('authorization', bearerToken)
                 .end((err, res) => {
                     expect(res).to.have.status(404)
                     expect(res.text).to.equal('Customer not found')
@@ -166,6 +239,7 @@ describe('/customer', () => {
         it('Handle non-integer id', done => {
             chai.request(app)
                 .delete(`/customer/a`)
+                .set('authorization', bearerToken)
                 .end((err, res) => {
                     expect(res).to.have.status(400)
                     expect(res.text).to.equal('"id" must be a number')
@@ -177,6 +251,7 @@ describe('/customer', () => {
         it('Get correct customer', done => {
             chai.request(app)
                 .get(`/customer/${testCustomer.customer_id}`)
+                .set('authorization', bearerToken)
                 .end((err, res) => {
                     expect(res).to.have.status(200)
                     expect(res.body).to.deep.equal(testCustomer)
@@ -186,6 +261,7 @@ describe('/customer', () => {
         it('Check the deleted customer is non-existent', done => {
             chai.request(app)
                 .get(`/customer/${newCustomerId}`)
+                .set('authorization', bearerToken)
                 .end((err, res) => {
                     expect(res).to.have.status(404)
                     expect(res.text).to.equal('Customer not found')
@@ -195,6 +271,7 @@ describe('/customer', () => {
         it('Handle non-integer id', done => {
             chai.request(app)
                 .get(`/customer/a`)
+                .set('authorization', bearerToken)
                 .end((err, res) => {
                     expect(res).to.have.status(400)
                     expect(res.text).to.equal('"id" must be a number')
